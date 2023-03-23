14e64a3cfdd372f29fb297536f2f9911fa82e4ae
Marvin Vogt <git@srv6d.space>
2023-03-23 22:45:06 +0000

    Implement options

--- src/pyenv/install.sh
+++ src/pyenv/install.sh
@@ -14,4 +14,16 @@ echo 'export PYENV_ROOT="$HOME/.pyenv"' >> ~/.bashrc
 echo 'command -v pyenv >/dev/null || export PATH="$PYENV_ROOT/bin:$PATH"' >> ~/.bashrc
 echo 'eval "$(pyenv init -)"' >> ~/.bashrc
 
+if [ $VERSION != "latest" ]; then
+    export PYENV_GIT_TAG=v$VERSION
+fi
+
 curl -s -S -L https://raw.githubusercontent.com/pyenv/pyenv-installer/master/bin/pyenv-installer | bash
+
+if [ $INSTALL != "None" ]; then
+    read -a VERSIONS <<< "$INSTALL"
+    for version in "${VERSIONS[@]}";
+    do
+        pyenv install $version
+    done
+fi
